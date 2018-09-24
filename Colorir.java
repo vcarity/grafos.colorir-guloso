5a6ebef894ef57e68d6117fdf1bfb19bc0b51911
b1ckw <victorcfsc@dcomp.ufs.br>
2018-09-23 21:54:47 -0300

    Terminei

--- Colorir.java
+++ Colorir.java
@@ -9,12 +9,47 @@ public class Colorir {
     int[] resultado = new int[grafo.numeroDeVertices];
     boolean[] corDisponivel = new boolean[grafo.numeroDeVertices];
 
-    for (int i = 0; i < resultado.length; i++) {
+    for (int i = 0; i < grafo.numeroDeVertices; i++) {
       resultado[i] = -1;
       corDisponivel[i] = true;
     }
+    resultado[0] = 0;
 
-    for (int i = 0; i < resultado.length; i++) {
+    for (int i = 1; i < grafo.numeroDeVertices; i++) {
+
+      if (resultado[i] == -1) {
+
+        for (int j = 0; j < grafo.numeroDeVertices; j++) {
+
+          if (grafo.grafo[i][j] == 1) {
+
+            corDisponivel[j] = false;
+
+          }
+
+        }
+
+        for (int cor = 0; cor < grafo.numeroDeVertices; cor++) {
+
+          if(corDisponivel[cor]) {
+
+            resultado[i] = cor;
+
+          }
+
+        }
+
+        for (int k = 0; k < grafo.numeroDeVertices; k++) {
+
+          corDisponivel[k] = true;
+
+        }
+
+      }
+
+    }
+
+    for (int i = 0; i < grafo.numeroDeVertices; i++) {
       System.out.println("Vértice " + i + " cor " + resultado[i]);
     }
 
